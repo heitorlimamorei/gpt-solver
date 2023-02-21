47265034e1f5b86634a4f64a0ff65cd0d86c7f58
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2023-02-21 12:30:47 -0300

    CORREÇÃO na tela de login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,15 +3,15 @@ import CabecalhoHome from "../components/home/CabecalhoHome";
 import useAppData from "../data/hook/useAppData";
 
 function Login() {
-    const {tema, alternarTema} = useAppData()
-    return (
-        <div className={`${tema} w-full h-full dark:bg-[#232323] bg-[#E0E5EC]`}>
-            <div className=" w-full transition-all duration-500 ease-linear dark:bg-[#232323] bg-[#E0E5EC] pb-[4.15rem]">
-                <CabecalhoHome/>
-                <BodyHome/>
-            </div>
-        </div>    
-    );
+  const { tema, alternarTema } = useAppData();
+  return (
+    <div className={`${tema} w-full h-full dark:bg-[#232323] bg-[#E0E5EC]`}>
+      <div className=" w-full transition-all duration-500 ease-linear dark:bg-[#232323] bg-[#E0E5EC] pb-[4.15rem]">
+        <CabecalhoHome tema={tema} alternarTema={alternarTema} />
+        <BodyHome />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
