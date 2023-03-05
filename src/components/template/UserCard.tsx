06c76cdf45e651ca8309c96eb3e290e57f3b6e0d
Felipe Rese <feliperese2018@gmail.com>
2023-03-04 22:27:43 -0300

    Ajuste do estilo para o modal de edição de usuários 	modified:   src/components/template/ManageUsers.tsx 	modified:   src/components/template/ModalForm.tsx 	modified:   src/components/template/UserCard.tsx

--- src/components/template/UserCard.tsx
+++ src/components/template/UserCard.tsx
@@ -8,15 +8,26 @@ interface UserCardProps {
   setCurrentEditingUser: (user: userProps) => void;
   setEditMode: (mode: boolean) => void;
 }
+
+function Capitalize(str){
+  return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
 function UserCard(props: UserCardProps) {
   const { user, deleteUser, setCurrentEditingUser, setEditMode } = props;
   return (
     <li
-      className="flex flex-row my-3 rounded-xl items-center transition-all duration-500 ease-linear bg-gradient-to-br from-[#FFFFFF] to-[#B8BCC2] dark:from-[#2A2A2A] dark:to-[#1C1C1C] shadow-[4.5px_4.5px_10px_#f6f7fb,_-4.5px_-4.5px_10px_#FFFFFF]
-              dark:shadow-[8px_8px_3px_#1C1C1C,_-3px_-3px_16px_#2A2A2A]"
+      className={user.role === "owner" ? "flex flex-row my-3 rounded-xl items-center bg-gradient-to-br from-[#FFFFFF] to-[#B8BCC2] dark:from-[#2A2A2A] dark:to-[#1C1C1C] shadow-[inset_5px_5px_15px_#696c6f,inset_-5px_-5px_15px_#ffffff] dark:shadow-[inset_5px_5px_10px_#101010,inset_-5px_-5px_10px_#363636] h-[4rem]" : 
+      `flex flex-row my-3 rounded-xl items-center bg-gradient-to-br from-[#FFFFFF] to-[#B8BCC2] dark:from-[#2A2A2A] dark:to-[#1C1C1C] shadow-[5px_5px_10px_#696c6f,-5px_-5px_10px_#ffffff]
+      dark:shadow-[8px_8px_3px_#1C1C1C,_-3px_-3px_16px_#2A2A2A] h-[4rem]`}
     >
       <h1 className="mx-4 w-[80%]">{user.email}</h1>
-      <p>{user.role}</p>
+      <p className={
+        user.role === "owner" ? "text-red-600 font-bold" : 
+        user.role === "admin" ? "text-green-600 font-bold" : 
+        user.role === "editor" ? "text-blue-600 font-bold" : 
+        "text-gray-600 font-bold"
+      }>{Capitalize(user.role)}</p>
       <div className="w-full flex flex-row items-end justify-end">
         {user.role === "owner" ? (
           <></>
@@ -26,7 +37,7 @@ function UserCard(props: UserCardProps) {
               iconClassName="text-red-600"
               ClassName="dark:bg-[#232323] bg-[#E0E5EC] 
                   dark:shadow-[3px_3px_12px_#0e0e0e,-3px_-3px_12px_#383838]
-                  shadow-[3px_3px_12px_#727578,-3px_-3px_12px_#ffffff] rounded-xl p-1 justify-self-end mr-5 my-1"
+                  shadow-[5px_5px_10px_#696c6f,-5px_-5px_10px_#ffffff] rounded-xl p-1 justify-self-end mr-5 my-1"
               onClick={
                 user.role === "owner"
                   ? false
@@ -39,7 +50,7 @@ function UserCard(props: UserCardProps) {
               iconClassName="text-green-600"
               ClassName="dark:bg-[#232323] bg-[#E0E5EC] 
                   dark:shadow-[3px_3px_12px_#0e0e0e,-3px_-3px_12px_#383838]
-                  shadow-[3px_3px_12px_#727578,-3px_-3px_12px_#ffffff] rounded-xl p-1 justify-self-end mr-5 my-1"
+                  shadow-[5px_5px_10px_#696c6f,-5px_-5px_10px_#ffffff] rounded-xl p-1 justify-self-end mr-5 my-1"
               onClick={
                 user.role === "owner"
                   ? false
