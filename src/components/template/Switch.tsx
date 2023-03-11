a6df8ad4c8b2892a580584be601ac4cb6e509aff
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2023-03-10 23:29:31 -0300

    Revert "correção do switch de manage user / props"
    This reverts commit 2075b8ce05ba66b9bbecfa2a969606dfe3a674fc.

--- src/components/template/Switch.tsx
+++ src/components/template/Switch.tsx
@@ -11,12 +11,14 @@ const Switch = (props:SwitchProps) => {
     setSelected(selection);
   };
   return (
-    <div className={`flex justify-start m-2 mb-3 items-center ${clasNameProp}`}>
+    <div className={`flex justify-center items-center ${clasNameProp}`}>
       <button
         className={`px-4 py-2 rounded-l-lg ${
           selected === "users"
-            ? "dark:bg-[#232323] dark:shadow-[inset_7px_7px_14px_#0e0e0e,inset_-7px_-7px_14px_#383838] bg-[#E0ECE5] shadow-[inset_-7px_7px_14px_#5a5c5e,inset_7px_-7px_14px_#ffffff] dark:text-white"
-            : "dark:bg-[#232323] dark:shadow-[7px_7px_14px_#0e0e0e,-7px_-7px_14px_#383838] dark:text-white bg-[#E0ECE5] shadow-[-7px_7px_14px_#5a5c5e,7px_-7px_14px_#ffffff]"
+            ? `dark:shadow-[inset_7px_7px_7px_#0e0e0e,inset_-7px_-7px_7px_#383838] dark:bg-[#232323]  dark:text-white
+            shadow-[inset_7px_7px_7px_#5a5c5e,inset_-7px_-7px_7px_#ffffff]`
+            : `dark:shadow-[7px_7px_7px_#0e0e0e,-7px_-7px_7px_#383838] dark:bg-[#232323] dark:text-white text-gray-7004
+            shadow-[7px_7px_12px_#5a5c5e,-7px_-7px_12px_#ffffff] bg-[#E0E5EC]`
         }`}
         onClick={() => handleSelection("users")}
       >
@@ -25,8 +27,10 @@ const Switch = (props:SwitchProps) => {
       <button
         className={`px-4 py-2 rounded-r-lg ${
           selected === "properties"
-            ? "dark:bg-[#232323] dark:shadow-[inset_7px_7px_14px_#0e0e0e,inset_-7px_-7px_14px_#383838] bg-[#E0ECE5] shadow-[inset_-7px_7px_14px_#5a5c5e,inset_7px_-7px_14px_#ffffff] dark:text-white"
-            : "dark:bg-[#232323] dark:shadow-[-7px_7px_14px_#0e0e0e,7px_-7px_14px_#383838] dark:text-white bg-[#E0ECE5] shadow-[-7px_7px_14px_#5a5c5e,7px_-7px_14px_#ffffff]"
+            ? `dark:shadow-[inset_7px_7px_7px_#0e0e0e,inset_-7px_-7px_7px_#383838] dark:bg-[#232323] dark:text-white
+            shadow-[inset_7px_7px_7px_#5a5c5e,inset_-7px_-7px_7px_#ffffff]`
+            : `dark:shadow-[7px_7px_7px_#0e0e0e,-7px_-7px_7px_#383838] dark:bg-[#232323] dark:text-white text-gray-700
+            shadow-[7px_7px_12px_#5a5c5e,-7px_-7px_12px_#ffffff] bg-[#E0E5EC]`
         }`}
         onClick={() => handleSelection("properties")}
       >
