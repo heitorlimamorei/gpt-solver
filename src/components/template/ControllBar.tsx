49a1b9acba713ca04f90e3b21d6aaf574af53438
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2023-04-01 10:38:01 -0300

    fix: remoção temp do botão de download pdf

--- src/components/template/ControllBar.tsx
+++ src/components/template/ControllBar.tsx
@@ -47,18 +47,6 @@ function ControllBar(props: ControllBarProps) {
             <p>{filterIcon(8)}</p>
           </Button>
         )}
-         {sheet.session.canManageSheetProps && (
-          <Button
-            ClassName="px-2 py-1 mt-1 rounded-lg ml-2 flex justify-center dark:text-white w-[15%] h-[3rem] md:w-[3%]"
-            onClick={() => generatePDFReport(sheet, sumAllItems, getStatistics )}
-            // text="Filtros"
-            // textClassName="px-4 py-2 font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#0085FF] to-[#1400FF] dark:bg-gradient-to-r dark:from-[#00F0FF] dark:to-[#00A5BC]"
-            classChildren="dark:text-[#00F0FF] text-[#0085FF]"
-            iconClassName="dark:text-[#00F0FF] text-[#0085FF]"
-          >
-            <p>{dowloadIcon}</p>
-          </Button>
-        )}
       </div>
     </div>
   );
