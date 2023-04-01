ad85fdf268b63dcb223ca1a0768dc6b4788cc290
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2023-04-01 10:36:38 -0300

    feat: Adição do serviço de PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  scope: "/",
+  sw: "sw.js",
+});
+
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+});
---
+++ src/pages/_document.tsx
@@ -0,0 +1,44 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    const themeColor = "#333333"; // Defina a cor do tema aqui
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          />
+          <link rel="icon" href="/logo180x180.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/logo180x180.png" />
+          {/* Defina a cor da barra de endereço para o Microsoft Edge */}
+          <meta name="msapplication-TileColor" content={themeColor} />
+
+          {/* Defina a cor da barra de endereço para navegadores que não suportam a tag `meta[name=msapplication-TileColor]` */}
+          <meta name="theme-color" content={themeColor} />
+          <style>{`body { background-color: ${themeColor}; }`}</style>
+          <meta name="description" content="Minha descrição aqui" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
