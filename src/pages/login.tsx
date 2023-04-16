d66c95b50ccdcafa4fef7c668c04c9ad7219b8e7
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2023-04-16 18:34:50 -0300

    fix:a real litle fix

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,7 +5,7 @@ import useAppData from "../data/hook/useAppData";
 function Login() {
   const { tema, alternarTema } = useAppData();
   return (
-    <div className={`${tema} w-full h-full dark:bg-[#232323] bg-[#E0E5EC]`}>
+    <div className={`dark w-full h-full dark:bg-[#232323] bg-[#E0E5EC]`}>
       <div className=" w-full transition-all duration-500 ease-linear dark:bg-[#232323] bg-[#E0E5EC] pb-[4.15rem]">
         <CabecalhoHome tema={tema} alternarTema={alternarTema} />
         <BodyHome />
