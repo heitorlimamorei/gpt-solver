90d24cf5fb692f80fb06a9f59cbc2e879358779a
Felipe Rese <feliperese2018@gmail.com>
2023-08-11 22:31:46 -0300

    vai

---
+++ src/pages/test.jsx
@@ -0,0 +1,13 @@
+import Layout from '../components/template/Layout'
+import Feedback from '../components/template/feedback';
+function test() {
+    return ( 
+        <Layout>
+            <div className='w-full h-[87vh]'>
+                <Feedback></Feedback>
+            </div>
+        </Layout>
+     );
+}
+
+export default test;
