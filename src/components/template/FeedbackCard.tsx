371fcd8041117aed483a630e69e95c3d1cee2f02
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2023-08-20 12:39:39 -0300

    feat and chore: upgrade of survey system and add of env BASE_URL

--- src/components/template/FeedbackCard.tsx
+++ src/components/template/FeedbackCard.tsx
@@ -5,18 +5,20 @@ interface IFeedBackCardProps {
   userName: string;
   review: string;
   userUrl: string;
+  rating: number;
 }
 
 const FeedbackCard = (props:IFeedBackCardProps)  => {
-  const { userName, review, userUrl } = props;
+  const { userName, review, userUrl, rating } = props;
   return (
-    <li className="w-full h-1/6 rounded-xl p-3 dark:shadow-[8px_8px_25px_#141414,-8px_-8px_25px_#323232] shadow-[28px_28px_57px_#5a5a5a,-28px_-28px_57px_#ffffff]">
-      <div className="flex flex-row items-center w-full h-1/3">
+    <li className="w-full max-h-1/5 rounded-xl p-3 mb-5 dark:shadow-[3px_3px_25px_#141414,-3px_-3px_25px_#323232] shadow-[5px_5px_25px_#5a5a5a,-5px_-5px_25px_#ffffff]">
+      <div className="flex flex-row items-center w-full mb-4  h-1/3">
         <img className="w-[40px] h-[40px] rounded-full " src={userUrl} />
-        <p className="ml-2">{`${userName}`}</p>
-        <div className="mb-1">{StarIcon(6)}</div>
+        <p className="ml-4 mr-3">{`${userName}`}</p>
+        <p>{`${rating}`}</p>
+        <div className="">{StarIcon(6)}</div> 
       </div>
-      <div className="mt-2">
+      <div className="m-2">
         <p className="m-1">{review}</p>
       </div>
     </li>
--- src/pages/surveys/feedback/reviews.tsx
+++ src/pages/surveys/feedback/reviews.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
+import Layout from "../../../components/template/Layout";	
 import FeedbackCard from "../../../components/template/FeedbackCard";
 import axios from "axios";
 import variaveis from "../../../model/variaveis";
 
 interface IReviewsProps {
   id: string;
-  rating: number;
+  stars: number;
   text: string;
   user_url: string;
   user_name: string;
@@ -21,22 +22,25 @@ export default function Reviews() {
       setReviwes(response.data);
     });
   }, []);
-
+  console.log(reviwes)
   return (
+    <Layout titulo='' subtitulo="">
     <div
-      className="max-w-xl  mx-auto  my-10 p-10 bg-[#E0E5EC] dark:bg-[#232323] rounded-xl dark:shadow-[12px_12px_32px_#0f0f0f,-12px_-12px_32px_#373737]
+      className="w-[50%] mx-auto  my-10 p-10 bg-[#E0E5EC] dark:bg-[#232323] rounded-xl dark:shadow-[12px_12px_32px_#0f0f0f,-12px_-12px_32px_#373737]
         shadow-[12px_12px_32px_#5e6063,-12px_-12px_32px_#ffffff] dark:text-white"
     >
-      <ul className=" p-4 flex flex-col w-full h-[50vh] bg-[#E0E5EC] dark:bg-[#232323] rounded-xl dark:shadow-[inset_8px_8px_25px_#141414,inset_-8px_-8px_25px_#323232] shadow-[inset_7px_7px_21px_#5a5a5a,inset_-7px_-7px_21px_#ffffff]">
+      <ul className="overflow-y-scroll p-4 flex flex-col w-full h-[50vh] bg-[#E0E5EC] dark:bg-[#232323] rounded-xl dark:shadow-[inset_8px_8px_25px_#141414,inset_-8px_-8px_25px_#323232] shadow-[inset_7px_7px_21px_#5a5a5a,inset_-7px_-7px_21px_#ffffff]">
         {reviwes.map((review) => (
           <FeedbackCard
             key={review.id}
             userName={review.user_name}
             review={review.text}
             userUrl={review.user_url}
+            rating={review.stars}
           />
         ))}
       </ul>
     </div>
+    </Layout>
   );
 }
