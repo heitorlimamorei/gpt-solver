a5a0e1fadec3457b340ff78713da6cfe29e37195
felipeRese <feliperese2018@gmail.com>
2023-08-30 19:42:21 -0300

    feat:Home page improved & changes on scroll bar 	new file:   public/images/homeimage.png 	modified:   src/components/home/BodyHome.tsx 	modified:   src/components/home/CabecalhoHome.tsx 	modified:   src/pages/index.tsx 	modified:   src/pages/login.tsx 	modified:   src/styles/globals.css

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,8 +5,8 @@ import useAppData from "../data/hook/useAppData";
 function Login() {
   const { tema, alternarTema } = useAppData();
   return (
-    <div className={`dark w-full h-full dark:bg-[#232323] bg-[#E0E5EC]`}>
-      <div className=" w-full h-full transition-all duration-500 ease-linear dark:bg-[#232323] bg-[#E0E5EC]">
+    <div className={`dark w-full h-[100vh] dark:bg-[#232323] bg-[#E0E5EC]`}>
+      <div className=" w-full h-[100vh] transition-all duration-500 ease-linear dark:bg-[#232323] bg-[#E0E5EC]">
         <CabecalhoHome tema={tema} alternarTema={alternarTema} />
         <BodyHome />
       </div>
