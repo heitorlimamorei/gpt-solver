65f5411288b2bf9c39782c723b234ab52e76a565
heitorlimamorei <heitorlmoreira2017@outlook.com>
2023-09-05 20:29:07 +0000

    fixing the switch logic and romeving the switch from desktop version

--- src/components/template/IncomeOutcomeSwitch.tsx
+++ src/components/template/IncomeOutcomeSwitch.tsx
@@ -1,38 +1,36 @@
-import React, { useState } from "react";
+import { memo } from "react";
 
-function IncomeOutcomeSwitch( {reference, onChange}) {
-  const [checked, setChecked] = useState(false);
+interface IIncomeOutcomeSwitchProps {
+  isChecked: boolean;
+  toggleChecked: () => void;
+};
 
-  const handleChange = (e) => {
-    setChecked(e.target.checked);
+const IncomeOutcomeSwitch = ({ isChecked, toggleChecked }: IIncomeOutcomeSwitchProps) => {
+  const handleChange = () => {
+    toggleChecked()
   };
 
   return (
-    <div className="flex  items-center justify-center mr-2 md:w-[15%] w-[25%] h-full dark:shadow-[5px_5px_10px_#0e0e0e,-5px_-5px_10px_#383838] rounded-full text-white">
+    <div className="md:hidden flex  items-center justify-center mr-2 md:w-[15%] w-[25%] h-full dark:shadow-[5px_5px_10px_#0e0e0e,-5px_-5px_10px_#383838] rounded-full text-white">
       <label
         htmlFor="toggle"
-        className={`w-full h-9 hover:cursor-pointer flex items-center rounded-full p-1 transition-colors duration-300 ease-in-out ${
-          checked ? "bg-red-500" : "bg-green-500"
-        } nm-flat-gray-300`}
+        className={`w-full h-9 hover:cursor-pointer flex items-center rounded-full p-1 transition-colors duration-300 ease-in-out ${isChecked ? "bg-red-500" : "bg-green-500"
+          } nm-flat-gray-300`}
       >
         <input
           id="toggle"
           type="checkbox"
           className="hidden"
-          checked={checked}
-          onChange={(event) => {
-            onChange(),
-            handleChange(event)
-          }}
+          checked={isChecked}
+          onChange={handleChange}
         />
         <span
-          className={`flex items-center justify-center w-[53%] h-[100%] bg-[#232323] rounded-full shadow-md transform transition-transform duration-300 ease-in-out ${
-            checked ? "sm:translate-x-7 translate-x-5"  : "translate-x-0"
-          } nm-concave-gray-300`}
-        >{checked ? (<p>-</p>) : '+'}</span>
+          className={`flex items-center justify-center w-[53%] h-[100%] bg-[#232323] rounded-full shadow-md transform transition-transform duration-300 ease-in-out ${isChecked ? "sm:translate-x-7 translate-x-5" : "translate-x-0"
+            } nm-concave-gray-300`}
+        >{isChecked ? '-' : '+'}</span>
       </label>
     </div>
   );
 }
 
-export default IncomeOutcomeSwitch;
+export default memo(IncomeOutcomeSwitch);
