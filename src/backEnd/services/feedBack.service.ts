06621dfd53cd6b63329e59c28142ab6262fbd41c
heitorlimamorei <heitorlmoreira2017@outlook.com>
2023-09-11 14:24:07 +0000

    feat: add of aditional fields to the survey feedback

--- src/backEnd/services/feedBack.service.ts
+++ src/backEnd/services/feedBack.service.ts
@@ -2,9 +2,9 @@ import feedbackRepository from "../repositories/feedback.repository";
 import { INewFeedBackProps } from "../../types/feedBackTypes";
 
 async function createFeedBack(newBack: INewFeedBackProps) {
-    const { text, user_name, user_url, stars} = newBack;
+    const { text, user_name, user_url, stars, did_pan_before, financial_management_improved } = newBack;
 
-    if(!!text && !!user_name && !!user_url && !!stars) {
+    if (!!text && !!user_name && !!user_url && !!stars && !!did_pan_before && !!financial_management_improved) {
         await feedbackRepository.createFeedBack(newBack);
     } else {
         throw new Error(`Feedback could not be created`);
