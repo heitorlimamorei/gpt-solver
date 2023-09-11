e46760d69a6b862458c726592eb01beab07d42ba
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2023-09-11 11:37:11 -0300

    style: coding formating

--- src/backEnd/services/feedBack.service.ts
+++ src/backEnd/services/feedBack.service.ts
@@ -2,13 +2,28 @@ import feedbackRepository from "../repositories/feedback.repository";
 import { INewFeedBackProps } from "../../types/feedBackTypes";
 
 async function createFeedBack(newBack: INewFeedBackProps) {
-    const { text, user_name, user_url, stars, did_pan_before, financial_management_improved } = newBack;
+  const {
+    text,
+    user_name,
+    user_url,
+    stars,
+    did_pan_before,
+    financial_management_improved,
+  } = newBack;
 
-    if (!!text && !!user_name && !!user_url && !!stars && !!did_pan_before && !!financial_management_improved) {
-        await feedbackRepository.createFeedBack(newBack);
-    } else {
-        throw new Error(`Feedback could not be created`);
-    }
+  if (
+    !!text &&
+    !!user_name &&
+    !!user_url &&
+    !!stars &&
+    !!financial_management_improved &&
+    did_pan_before != undefined &&
+    did_pan_before != null
+  ) {
+    await feedbackRepository.createFeedBack(newBack);
+  } else {
+    throw new Error(`Feedback could not be created`);
+  }
 }
 
 async function getFeedBacks() {
