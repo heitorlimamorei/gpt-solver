b08682c58607e42627846bcb3e75d15c8d292e07
heitorlimamorei <heitorlmoreira2017@outlook.com>
2023-09-13 16:01:12 +0000

    feat: implementation of render options on the time line feed and soooo many refactors on all render time line feed flow and many variables name has been renamed.

--- src/components/ItemsTimeLine/Item.tsx
+++ src/components/ItemsTimeLine/Item.tsx
@@ -1,22 +1,10 @@
-import { memo } from "react";
-import { sheetItemProps } from "../../types/sheetTypes";
-import Button from "../Button";
-import useSheets from "../../data/hook/useSheets";
-import {
-  trashIcon,
-  editIcon,
-  ArrowUpIcon,
-  ArrouDownIcon,
-} from "../icons/Icones";
+import { memo } from 'react';
+import { sheetItemProps } from '../../types/sheetTypes';
+import Button from '../Button';
+import useSheets from '../../data/hook/useSheets';
+import { trashIcon, editIcon, ArrowUpIcon, ArrouDownIcon } from '../icons/Icones';
 
-const Item = ({
-  item,
-  setEditMode,
-}: {
-  item: sheetItemProps;
-  setEditMode: (c: sheetItemProps) => void;
-}) => {
-  const { sheet, deleteItem } = useSheets();
+const Item = ({ item }: { item: sheetItemProps }) => {
   return (
     <li className="h-14 my-3 w-full dark:text-white bg-[#e0e5ec] dark:bg-[#232323] shadow-[10px_10px_16px_#7c797f,-10px_-10px_16px_#ffffff] dark:shadow-[10px_10px_16px_#131313,-10px_-10px_16px_#333333] rounded-md">
       <div className="flex items-center w-full h-full ml-2 ">
@@ -33,21 +21,13 @@ const Item = ({
 
           <div className="ml-2">
             <h1 className="font-bold">{item.name}</h1>
-            <h2 className="text-sm font-light">Autor: {item.author.split("@")[0]}</h2>
+            <h2 className="text-sm font-light">Autor: {item.author.split('@')[0]}</h2>
           </div>
         </div>
         <div className="h-full w-[20%] flex items-center justify-end">
-          <p
-            className={`font-bold ${
-              item.value < 0 ? `text-red-600` : `text-green-600`
-            }`}
-          >
+          <p className={`font-bold ${item.value < 0 ? `text-red-600` : `text-green-600`}`}>
             R$
-            {item.value < 0 ? (
-              item.value * -1
-            ) : (
-              item.value
-            )}
+            {item.value < 0 ? item.value * -1 : item.value}
           </p>
         </div>
       </div>
