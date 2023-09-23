27f6a64aa2a8d1846ebe97150103235d61207a04
heitorlimamorei <heitorlmoreira2017@outlook.com>
2023-09-23 12:52:06 +0000

    fix: fixing some problem with survey's api

--- src/backEnd/services/feedbackSurveyStatistics.service.ts
+++ src/backEnd/services/feedbackSurveyStatistics.service.ts
@@ -8,6 +8,7 @@ import feedbackService from './feedBack.service';
 interface ISurveyDataProps {
   appHasBeenSharedData: boolean[];
   didPanBeforeData: boolean[];
+  continued_using: boolean[];
   starsData: number[];
   financialManagementImprovedData: number[];
 }
@@ -17,6 +18,7 @@ async function getSurveyData(): Promise<ISurveyDataProps> {
 
   const appHasBeenShared: boolean[] = transformArrayByField(feedback, 'appHasBeenShared');
   const did_pan_before: boolean[] = transformArrayByField(feedback, 'did_pan_before');
+  const continued_using: boolean[] = transformArrayByField(feedback, 'continued_using');
   const stars: number[] = transformArrayByField(feedback, 'stars');
   const financial_management_improved: number[] = transformArrayByField(
     feedback,
@@ -26,6 +28,7 @@ async function getSurveyData(): Promise<ISurveyDataProps> {
   return {
     appHasBeenSharedData: appHasBeenShared,
     didPanBeforeData: did_pan_before,
+    continued_using: continued_using,
     starsData: stars,
     financialManagementImprovedData: financial_management_improved,
   };
@@ -45,15 +48,19 @@ function getSuverysMeans(surveyData: ISurveyDataProps) {
 
 function getPercentData(surveyData: ISurveyDataProps) {
   const did_pan_beforeSliced = sliceByBooleanFeature(surveyData.didPanBeforeData);
+  const continued_usingSliced = sliceByBooleanFeature(surveyData.continued_using);
+  const appHasBeenSharedSliced = sliceByBooleanFeature(surveyData.appHasBeenSharedData);
+
   const did_pan_beforePercent =
     did_pan_beforeSliced.postive.length / surveyData.didPanBeforeData.length;
-  const appHasBeenSharedSliced = sliceByBooleanFeature(surveyData.appHasBeenSharedData);
   const appHasBeenSharedPercent =
     appHasBeenSharedSliced.postive.length / surveyData.appHasBeenSharedData.length;
+  const continued_usingPercent = continued_usingSliced.postive.length / surveyData.continued_using.length;
 
   return {
     did_pan_beforePercent: did_pan_beforePercent * 100,
     appHasBeenSharedPercent: appHasBeenSharedPercent * 100,
+    continued_usingPercent: continued_usingPercent * 100,
   };
 }
 
