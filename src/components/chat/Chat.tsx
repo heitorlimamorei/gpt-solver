991bf43159f4fb84e54d3892b480de0029c6cea5
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-03-07 20:24:09 -0300

    feat: add  replacing of jsx tag on message

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -15,9 +15,18 @@ interface IChatProps {
 }
 
 export default function Chat(props: IChatProps) {
+  function replaceCode(messages: IMessage[]): IMessage[] {
+    props.messages.map((message: IMessage) => {
+      if (message.role === 'assistant') {
+        message.content = message.content.replace('```jsx', '```ts')
+      }
+    });
+    return messages;
+  }
+
   return (
     <ul className="flex flex-col md:items-center w-full h-full px-3 sm:px-20 md:px-72 overflow-y-scroll">
-      {props.messages.map((m, index) => {
+      {replaceCode(props.messages).map((m, index) => {
         return (
           <li key={index} className="flex flex-col w-full px-3 mt-7">
             <div className="flex flex-row">
@@ -25,11 +34,13 @@ export default function Chat(props: IChatProps) {
                 width={30}
                 height={30}
                 alt="Ai logo"
-                src={m.role == 'system' || m.role == "assistant" ? Logo : Profile}
+                src={
+                  m.role == 'system' || m.role == 'assistant' ? Logo : Profile
+                }
                 className="mr-5 rounded-full"></Image>
-              <p className="font-bold self-center bg-transparent">
-                {m.role == 'system' ||  m.role == "assistant" ? 'AI' : 'Você'}
-              </p>
+              <div className="font-bold self-center bg-transparent">
+                {m.role == 'system' || m.role == 'assistant' ? 'AI' : 'Você'}
+              </div>
             </div>
             <p className="mt-2 ml-[3rem] text-sm">{m.content}</p>
           </li>
