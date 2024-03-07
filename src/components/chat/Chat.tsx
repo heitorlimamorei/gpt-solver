e1b8780ee9e09590f0479d2d06c1f8db48a0e40d
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-03-07 19:32:02 -0300

    feat: add BFF integration

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -5,37 +5,36 @@ import Image from 'next/image';
 import Logo from '../../../public/ai.png';
 import Profile from '../../../public/profile.jpg';
 
-export interface ChatType {
+export interface IMessage {
   role: string;
   content: string;
 }
 
-interface ChatProps {
-  messages: Array<ChatType>;
+interface IChatProps {
+  messages: IMessage[];
 }
 
-export default function Chat(props: ChatProps) {
-  let color = false;
+export default function Chat(props: IChatProps) {
   return (
-    <div className="flex flex-col md:items-center w-full h-full sm:px-20 md:px-72">
+    <ul className="flex flex-col md:items-center w-full h-full px-3 sm:px-20 md:px-72 overflow-y-scroll">
       {props.messages.map((m, index) => {
         return (
-          <div key={index} className="flex flex-col w-full px-3 mt-7">
+          <li key={index} className="flex flex-col w-full px-3 mt-7">
             <div className="flex flex-row">
               <Image
                 width={30}
                 height={30}
                 alt="Ai logo"
-                src={m.role == 'system' ? Logo : Profile}
+                src={m.role == 'system' || m.role == "assistant" ? Logo : Profile}
                 className="mr-5 rounded-full"></Image>
               <p className="font-bold self-center bg-transparent">
-                {m.role == 'system' ? 'AI' : 'Você'}
+                {m.role == 'system' ||  m.role == "assistant" ? 'AI' : 'Você'}
               </p>
             </div>
             <p className="mt-2 ml-[3rem] text-sm">{m.content}</p>
-          </div>
+          </li>
         );
       })}
-    </div>
+    </ul>
   );
 }
---
+++ src/hooks/useChat.ts
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { useState } from "react";
+interface IMessage {
+    content: string;
+    role: string;
+}
+
+interface IUseChatResp {
+    messages: IMessage[];
+    addMessage(message: string): Promise<void>;
+}
+
+interface IBffResp {
+    conversation: IMessage[];
+    total_tokens: number;
+}
+
+const systemMessage = {
+    role: "system",
+    content: "Olá eu sou o GPT-SOLVER!",
+};
+
+export default function useChat(): IUseChatResp {
+    const [messages, setMessages] = useState<IMessage[]>([systemMessage]);
+    
+    const sendToBff = async (message: IMessage) => {
+      try {
+        const resp = await axios.post<IBffResp>(
+          'api/openai',
+          {
+            conversation: [...messages, message],
+            model: 'gpt-4',
+          },
+          {
+            baseURL: process.env.BASE_URL,
+          },
+        );
+        const { conversation, total_tokens } = resp.data;
+        setMessages(conversation);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    const addMessage = async (message: string) => {
+        const messageF = {
+            role: "user",
+            content: message,
+        }
+
+        setMessages((c) => [...c, messageF]);
+        await sendToBff(messageF);
+    };
+
+    return {
+        messages,
+        addMessage,
+    };
+}
