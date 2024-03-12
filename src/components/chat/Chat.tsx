f4d1dd958bb4fe94a4e90dda79235efe02b2e85a
Felipe Rese <feliperese2018@gmail.com>
2024-03-12 06:07:11 -0300

    feat: added ace-editor

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-
 import Image from 'next/image';
-
+import AceEditor from 'react-ace';
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/mode-java';
+import 'ace-builds/src-noconflict/mode-python';
+import 'ace-builds/src-noconflict/theme-github_dark';
 import Logo from '../../../public/ai.png';
 import Profile from '../../../public/profile.jpg';
 
@@ -15,37 +18,84 @@ interface IChatProps {
 }
 
 export default function Chat(props: IChatProps) {
-  function replaceCode(messages: IMessage[]): IMessage[] {
-    props.messages.map((message: IMessage) => {
-      if (message.role === 'assistant') {
-        message.content = message.content.replace('```jsx', '```ts')
+  function extractCodeBlocks(message: IMessage): React.ReactNode[] {
+    const { content } = message;
+    const codeRegex = /```(ts|js|java|python)(.*?)```/gs;
+
+    let modifiedContent: React.ReactNode[] = [];
+    let lastIndex = 0;
+    let match;
+    while ((match = codeRegex.exec(content)) !== null) {
+      const codeBlock = match[2].trim();
+      const language = match[1] === 'ts' ? 'typescript' : match[1];
+      const codeBlockWithMode = (
+        <AceEditor
+          key={`ace-${lastIndex}`}
+          placeholder="Placeholder Text"
+          mode={language}
+          theme="github_dark"
+          name="blah2"
+          fontSize={14}
+          showPrintMargin={true}
+          showGutter={true}
+          highlightActiveLine={true}
+          value={codeBlock}
+          setOptions={{
+            enableBasicAutocompletion: false,
+            enableLiveAutocompletion: false,
+            enableSnippets: false,
+            showLineNumbers: true,
+            tabSize: 2,
+          }}
+        />
+      );
+
+      // Text before code block
+      if (lastIndex !== match.index) {
+        modifiedContent.push(
+          <span key={`text-${lastIndex}`}>
+            {content.substring(lastIndex, match.index)}
+          </span>,
+        );
       }
-    });
-    return messages;
+
+      // Code block
+      modifiedContent.push(codeBlockWithMode);
+
+      lastIndex = match.index + match[0].length;
+    }
+
+    // Text after last code block
+    if (lastIndex < content.length) {
+      modifiedContent.push(
+        <span key={`text-${lastIndex}`}>{content.substring(lastIndex)}</span>,
+      );
+    }
+
+    return modifiedContent;
   }
 
   return (
     <ul className="flex flex-col md:items-center w-full h-full px-3 sm:px-20 md:px-72 overflow-y-scroll">
-      {replaceCode(props.messages).map((m, index) => {
-        return (
-          <li key={index} className="flex flex-col w-full px-3 mt-7">
-            <div className="flex flex-row">
-              <Image
-                width={30}
-                height={30}
-                alt="Ai logo"
-                src={
-                  m.role == 'system' || m.role == 'assistant' ? Logo : Profile
-                }
-                className="mr-5 rounded-full"></Image>
-              <div className="font-bold self-center bg-transparent">
-                {m.role == 'system' || m.role == 'assistant' ? 'AI' : 'Você'}
-              </div>
+      {props.messages.map((m, index) => (
+        <li key={index} className="flex flex-col w-full px-3 mt-7">
+          <div className="flex flex-row">
+            <Image
+              width={30}
+              height={30}
+              alt="Ai logo"
+              src={
+                m.role === 'system' || m.role === 'assistant' ? Logo : Profile
+              }
+              className="mr-5 rounded-full"
+            />
+            <div className="font-bold self-center bg-transparent">
+              {m.role === 'system' || m.role === 'assistant' ? 'AI' : 'Você'}
             </div>
-            <p className="mt-2 ml-[3rem] text-sm">{m.content}</p>
-          </li>
-        );
-      })}
+          </div>
+          <p className="mt-2 ml-[3rem] text-sm">{extractCodeBlocks(m)}</p>
+        </li>
+      ))}
     </ul>
   );
 }
