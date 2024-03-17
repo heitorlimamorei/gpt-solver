4e33982074bc4b0f00dd5cc4bf7aa6c5416a5309
Felipe Rese <feliperese2018@gmail.com>
2024-03-17 01:29:17 -0300

    feat: added markdowns

--- src/hooks/useFormat.tsx
+++ src/hooks/useFormat.tsx
@@ -80,35 +80,65 @@ function generateCodeBlockWithMode(
   );
 }
 
-function generateTextContent(
-  content: string,
-  lastIndex: number,
-  endIndex: number,
-): React.ReactNode {
-  return (
-    <span key={`text-${lastIndex}`} className="mx-3">
-      {content
-        .substring(lastIndex, endIndex)
-        .split('\n')
-        .map((line, index) => (
-          <React.Fragment key={`line-${index}`}>
-            {line}
-            <br />
-          </React.Fragment>
-        ))}
-    </span>
-  );
+function generateTextContent(content: string): React.ReactNode {
+  const boldRegex = /\*\*(.*?)\*\*|__(.*?)__/g;
+  const italicRegex = /\*(.*?)\*|_(.*?)_/g;
+  const strikethroughRegex = /~~(.*?)~~/g;
+  const linkRegex = /\[([^\]]+)\]\(([^)]+)\)/g;
+  const blockquoteRegex = /^>(.*)/gm;
+  const bulletListRegex = /^- (.*)/gm;
+  const numberedlistRegex = /^\d+\. (.*)/gm;
+  const h1Regex = /^# (.*)/gm;
+  const h2Regex = /^## (.*)/gm;
+  const h3Regex = /^### (.*)/gm;
+
+  const elements: React.ReactNode[] = [];
+
+  content.split('\n').forEach((line) => {
+    if (boldRegex.test(line)) {
+      line = line.replace(boldRegex, '<strong>$1$2</strong>');
+    }
+    if (italicRegex.test(line)) {
+      line = line.replace(italicRegex, '<em>$1$2</em>');
+    }
+    if (strikethroughRegex.test(line)) {
+      line = line.replace(strikethroughRegex, '<del>$1</del>');
+    }
+    if (linkRegex.test(line)) {
+      line = line.replace(linkRegex, '<a href="$2">$1</a>');
+    }
+    if (blockquoteRegex.test(line)) {
+      line = line.replace(blockquoteRegex, '<blockquote>$1</blockquote>');
+    }
+    if (bulletListRegex.test(line)) {
+      line = line.replace(bulletListRegex, '<li>$1</li>');
+      line = `<ul>${line}</ul>`;
+    }
+    if (numberedlistRegex.test(line)) {
+      line = line.replace(numberedlistRegex, '<li>$1</li>');
+      line = `<ol>${line}</ol>`;
+    }
+    if (h1Regex.test(line)) {
+      line = line.replace(h1Regex, '<h1>$1</h1>');
+    }
+    if (h2Regex.test(line)) {
+      line = line.replace(h2Regex, '<h2>$1</h2>');
+    }
+    if (h3Regex.test(line)) {
+      line = line.replace(h3Regex, '<h3>$1</h3>');
+    }
+
+    elements.push(<div dangerouslySetInnerHTML={{ __html: line }} />);
+  });
+
+  return elements;
 }
 
 export function useFormat(message: IMessage): React.ReactNode[] {
   const { content } = message;
-<<<<<<< HEAD
 
   const codeRegex =
     /```(typescript|tsx|jsx|html|css|go|javascript|java|python|bash||)(.*?)```/gs;
-=======
-  const codeRegex = /```(typescript|tsx|html|css|go|javascript|java|python)(.*?)```/gs;
->>>>>>> 904e852f648fe14c47aa7b7b922f4661e52b7aaa
 
   let modifiedContent: React.ReactNode[] = [];
   let lastIndex = 0;
@@ -118,12 +148,8 @@ export function useFormat(message: IMessage): React.ReactNode[] {
     const language = getLanguage(match);
 
     if (lastIndex !== match.index) {
-      modifiedContent.push(
-<<<<<<< HEAD
-        generateTextContent(content, lastIndex, match.index),
-=======
-        <span key={`text-${lastIndex}`}>{content.substring(lastIndex, match.index)}</span>,
->>>>>>> 904e852f648fe14c47aa7b7b922f4661e52b7aaa
+      modifiedContent = modifiedContent.concat(
+        generateTextContent(content.substring(lastIndex, match.index)),
       );
     }
 
@@ -133,23 +159,11 @@ export function useFormat(message: IMessage): React.ReactNode[] {
   }
 
   if (lastIndex < content.length) {
-<<<<<<< HEAD
     const remainingContent = content.substring(lastIndex);
-    // Check if remaining content contains AceEditor component
-    if (!remainingContent.includes('<AceEditor')) {
-      modifiedContent.push(
-        generateTextContent(content, lastIndex, content.length),
-      );
-    } else {
-      modifiedContent.push(
-        <span key={`text-${lastIndex}`} className="">
-          {remainingContent}
-        </span>,
-      );
-    }
-=======
-    modifiedContent.push(<span key={`text-${lastIndex}`}>{content.substring(lastIndex)}</span>);
->>>>>>> 904e852f648fe14c47aa7b7b922f4661e52b7aaa
+    modifiedContent = modifiedContent.concat(
+      generateTextContent(remainingContent),
+    );
   }
+
   return modifiedContent;
 }
