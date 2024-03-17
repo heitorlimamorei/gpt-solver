8ae68768dbe441bad12075ec70ca173117517f2b
Felipe Rese <feliperese2018@gmail.com>
2024-03-17 01:18:24 -0300

    fix: fixed ace editor size & code refactor

--- src/hooks/useFormat.tsx
+++ src/hooks/useFormat.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-javascript';
 import 'ace-builds/src-noconflict/mode-java';
@@ -6,11 +7,13 @@ import 'ace-builds/src-noconflict/mode-typescript';
 import 'ace-builds/src-noconflict/mode-golang';
 import 'ace-builds/src-noconflict/mode-html';
 import 'ace-builds/src-noconflict/mode-css';
+import 'ace-builds/src-noconflict/mode-xml';
 import 'ace-builds/src-noconflict/theme-monokai';
 
 import { IMessage } from '@/components/chat/Chat';
 
 enum Language {
+  bash = 'bash',
   typescript = 'typescript',
   tsx = 'typescript',
   jsx = 'javascript',
@@ -22,97 +25,119 @@ enum Language {
   css = 'css',
 }
 
+function getLanguage(match: RegExpMatchArray): Language {
+  switch (match[1] as Language) {
+    case Language.bash:
+      return Language.bash;
+    case Language.typescript:
+      return Language.typescript;
+    case Language.tsx:
+      return Language.tsx;
+    case Language.html:
+      return Language.html;
+    case Language.css:
+      return Language.css;
+    case Language.go:
+      return Language.go;
+    case Language.javascript:
+      return Language.javascript;
+    case Language.java:
+      return Language.java;
+    case Language.python:
+      return Language.python;
+    default:
+      return Language.javascript;
+  }
+}
+
+function generateCodeBlockWithMode(
+  codeBlock: string,
+  language: Language,
+): React.ReactNode {
+  const numberOfLines = codeBlock.split('\n').length;
+
+  return (
+    <AceEditor
+      width="100%"
+      height={`calc(20px * ${numberOfLines} + 40px)`}
+      placeholder="Placeholder Text"
+      mode={language}
+      theme="monokai"
+      name="blah2"
+      fontSize={12}
+      showPrintMargin={true}
+      showGutter={true}
+      highlightActiveLine={true}
+      value={codeBlock}
+      setOptions={{
+        enableBasicAutocompletion: true,
+        enableLiveAutocompletion: true,
+        enableSnippets: true,
+        showLineNumbers: true,
+        tabSize: 2,
+      }}
+    />
+  );
+}
+
+function generateTextContent(
+  content: string,
+  lastIndex: number,
+  endIndex: number,
+): React.ReactNode {
+  return (
+    <span key={`text-${lastIndex}`} className="mx-3">
+      {content
+        .substring(lastIndex, endIndex)
+        .split('\n')
+        .map((line, index) => (
+          <React.Fragment key={`line-${index}`}>
+            {line}
+            <br />
+          </React.Fragment>
+        ))}
+    </span>
+  );
+}
+
 export function useFormat(message: IMessage): React.ReactNode[] {
   const { content } = message;
+
   const codeRegex =
-    /```(typescript|tsx|html|css|go|javascript|java|python)(.*?)```/gs;
+    /```(typescript|tsx|jsx|html|css|go|javascript|java|python|bash||)(.*?)```/gs;
 
   let modifiedContent: React.ReactNode[] = [];
   let lastIndex = 0;
   let match;
   while ((match = codeRegex.exec(content)) !== null) {
     const codeBlock = match[2].trim();
-    let language: Language = Language.javascript;
-    switch (match[1] as Language) {
-      case Language.typescript:
-        language = Language.typescript;
-        break;
+    const language = getLanguage(match);
 
-      case Language.tsx:
-        language = Language.tsx;
-        break;
-
-      case Language.html:
-        language = Language.html;
-        break;
-
-      case Language.css:
-        language = Language.css;
-        break;
-
-      case Language.go:
-        language = Language.go;
-        break;
-
-      case Language.javascript:
-        language = Language.javascript;
-        break;
-
-      case Language.java:
-        language = Language.java;
-        break;
-
-      case Language.python:
-        language = Language.python;
-        break;
-
-      default:
-        language = Language.javascript; // Defina uma linguagem padrão
-        break;
-    }
-    const codeBlockWithMode = (
-      <AceEditor
-        key={`ace-${lastIndex}`}
-        placeholder="Placeholder Text"
-        mode={language}
-        theme="monokai"
-        name="blah2"
-        fontSize={14}
-        showPrintMargin={true}
-        showGutter={true}
-        highlightActiveLine={true}
-        value={codeBlock}
-        setOptions={{
-          enableBasicAutocompletion: true,
-          enableLiveAutocompletion: true,
-          enableSnippets: true,
-          showLineNumbers: true,
-          tabSize: 2,
-        }}
-      />
-    );
-
-    // Text before code block
     if (lastIndex !== match.index) {
       modifiedContent.push(
-        <span key={`text-${lastIndex}`}>
-          {content.substring(lastIndex, match.index)}
-        </span>,
+        generateTextContent(content, lastIndex, match.index),
       );
     }
 
-    // Code block
-    modifiedContent.push(codeBlockWithMode);
+    modifiedContent.push(generateCodeBlockWithMode(codeBlock, language));
 
     lastIndex = match.index + match[0].length;
   }
 
-  // Text after last code block
   if (lastIndex < content.length) {
-    modifiedContent.push(
-      <span key={`text-${lastIndex}`}>{content.substring(lastIndex)}</span>,
-    );
+    const remainingContent = content.substring(lastIndex);
+    // Check if remaining content contains AceEditor component
+    if (!remainingContent.includes('<AceEditor')) {
+      modifiedContent.push(
+        generateTextContent(content, lastIndex, content.length),
+      );
+    } else {
+      modifiedContent.push(
+        <span key={`text-${lastIndex}`} className="">
+          {remainingContent}
+        </span>,
+      );
+    }
   }
-
   return modifiedContent;
 }
