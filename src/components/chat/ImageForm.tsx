6e129f675dede8c0118b35c5da0cfad6745d6d4b
Felipe Rese <feliperese2018@gmail.com>
2024-03-29 15:02:46 -0300

    fix: lint

--- src/components/chat/ImageForm.tsx
+++ src/components/chat/ImageForm.tsx
@@ -3,69 +3,62 @@ import Modal from 'react-modal';
 import { useForm } from 'react-hook-form';
 import { IconEmbed } from '../Icons';
 
+// Definindo estilos para o modal
+const modalStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    transform: 'translate(-50%, -50%)',
+    width: '80%',
+    backgroundColor: 'rgb(31 41 55)',
+    border: 'none',
+    borderRadius: '20px'
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0.50)'
+  }
+};
+
 const ModalComponent = () => {
+  // Utilizando react-hook-form e useState
   const { register, handleSubmit } = useForm();
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [previewImages, setPreviewImages] = useState([]);
 
+  // Configurando elemento principal do Modal
   Modal.setAppElement('#root');
-  const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      transform: 'translate(-50%, -50%)',
-      width: '80%',
-      backgroundColor: 'rgb(31 41 55)',
-      border: 'none',
-      borderRadius: '20px',
-    },
-    overlay: {
-      backgroundColor: 'rgba(0, 0, 0, 0.50)',
-    },
-  };
 
+  // Função para converter o arquivo em Base64
   const convertToBase64 = (file: any) => {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
+      reader.readAsDataURL(file);
       reader.onload = () => resolve(reader.result);
       reader.onerror = reject;
-      reader.readAsDataURL(file);
     });
   };
 
+  // Função para manipular a mudança de arquivos
   const handleFileChange = async (event: any) => {
     const files = event.target.files;
-    const imagesData: any = [];
-
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
-      const base64 = await convertToBase64(file);
-      imagesData.push(base64);
-    }
-    console.log(imagesData)
-    setPreviewImages(imagesData);
+    const imagesData: any = await Promise.all(Array.from(files).map(file => convertToBase64(file)));
+    setPreviewImages(imagesData); 
   };
 
-  const onSubmit = handleSubmit(async (data, event) => {
-    event.preventDefault();
+  // Função para lidar com o envio dos dados
+  const onSubmit = handleSubmit((data, event) => {
+    event?.preventDefault();
+    
     if (previewImages.length === 0) {
       console.error('Nenhuma imagem selecionada.');
       return;
     }
   
-    const imagesBase64: string[] = [];
-  
-    for (let i = 0; i < previewImages.length; i++) {
-      const base64 = previewImages[i];
-      imagesBase64.push(base64);
-    }
-  
-    console.log('Imagens em base64:', imagesBase64[0]);
+    console.log('Imagens em base64:', previewImages);
     setModalIsOpen(false);
   });
-  
 
   return (
     <div className="flex items-center justify-center ml-2">
@@ -78,9 +71,11 @@ const ModalComponent = () => {
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={() => setModalIsOpen(false)}
-        style={customStyles}
+        style={modalStyles}
         contentLabel="Upload Images">
+
         <h1 className="text-xl font-bold mb-2">Imagens:</h1>
+        
         <div className="flex flex-wrap">
           {previewImages.map((imageData, index) => (
             <img
@@ -91,6 +86,7 @@ const ModalComponent = () => {
             />
           ))}
         </div>
+      
         <form className="flex flex-col" onSubmit={onSubmit}>
           <div className="mb-4 self-center">
             <label
@@ -108,20 +104,13 @@ const ModalComponent = () => {
               multiple
             />
           </div>
+
           <div className="flex items-center justify-between">
-            <button
-              className="bg-red-600 hover:bg-red-700 text-white font-bold mx-5 py-2 px-4 rounded"
-              type="button"
-              onClick={() => setModalIsOpen(false)}>
-              Fechar
-            </button>
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold mx-5 py-2 px-4 rounded"
-              type="submit">
-              Enviar
-            </button>
+            <button className="bg-red-600 hover:bg-red-700 text-white font-bold mx-5 py-2 px-4 rounded" type="button" onClick={() => setModalIsOpen(false)}>Fechar</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold mx-5 py-2 px-4 rounded" type="submit">Enviar</button>
           </div>
         </form>
+
       </Modal>
     </div>
   );
