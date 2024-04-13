319a5e59d5613eece323cf4bc300c439b7b75127
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-04-13 12:42:34 -0300

    style: fixing identation issues

--- src/hooks/usePDF.ts
+++ src/hooks/usePDF.ts
@@ -1,21 +1,21 @@
-import axios from "axios";
+import axios from 'axios';
 const api = 'https://gpt-solver-backend.onrender.com';
 
 export default function usePDF() {
-    async function ExtractText(pdf: File): Promise<string> {
-        const formData = new FormData();
-        formData.append('file', pdf);
+  async function ExtractText(pdf: File): Promise<string> {
+    const formData = new FormData();
+    formData.append('file', pdf);
 
-        const resp = await axios.post(`${api}/v1/pdf/text-extractor`, formData);
+    const resp = await axios.post(`${api}/v1/pdf/text-extractor`, formData);
 
-        if (resp.status !== 200) throw new Error("Error when extracting pdf data");
+    if (resp.status !== 200) throw new Error('Error when extracting pdf data');
 
-        const text = resp.data as string;
+    const text = resp.data as string;
 
-        return text;
-    }
+    return text;
+  }
 
-    return {
-        ExtractText
-    };
+  return {
+    ExtractText,
+  };
 }
