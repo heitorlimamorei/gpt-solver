63ef371015bf55c842d4c773c12794805cab0cb9
felipeRese <feliperese2018@gmail.com>
2024-05-01 18:04:33 -0300

    fix: switch to create chatPDF

--- src/components/CreateChatModal.tsx
+++ src/components/CreateChatModal.tsx
@@ -15,7 +15,7 @@ const api = 'https://gpt-solver-backend.onrender.com';
 
 const CreateChatModal = ({ isOpen, toggle }: IDarkModalProps) => {
   const [chatName, setChatName] = useState<string>('');
-  const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState(true);
   const [plan, setPlan] = useState('');
   const searchParams = useSearchParams();
 
@@ -55,35 +55,35 @@ const CreateChatModal = ({ isOpen, toggle }: IDarkModalProps) => {
   };
 
   const handleCheckboxChange = () => {
-    setChecked((c) => !c);
+    setChecked(!checked);
   };
 
   return (
-    <BaseModal toggle={toggle} handleSubmit={handleSubmit}>
-      <h2 className="text-lg font-bold mb-4">Configurar novo Chat</h2>
-
-      <input
-        type="text"
-        className="w-full rounded-md bg-gray-700 mb-4 text-white px-4 py-2 border border-gray-600 focus:outline-none focus:border-blue-500"
-        placeholder="Nome do Chat"
-        value={chatName}
-        onChange={(e) => setChatName(e.target.value)}
-      />
-
-      <div className={plan == 'ultimate' ? 'flex flex-col' : 'hidden'}>
+    <>
+      <BaseModal toggle={toggle} handleSubmit={handleSubmit}>
+        <div className="flex flex-row w-full items-center justify-between mb-4">
+          <div
+            onClick={handleCheckboxChange}
+            className={
+              plan == 'ultimate'
+                ? `flex flex-col self-start justify-self-start ${checked ? 'bg-zinc-400' : 'bg-green-500'} rounded-2xl hover:cursor-pointer py-[0.5rem] px-[1.5rem]`
+                : 'hidden'
+            }>
+            <div className="font-bold">PDF</div>
+          </div>
+          <div className="w-full flex items-center justify-center">
+            <h2 className="text-lg font-bold mr-14">Configurar novo Chat</h2>
+          </div>
+        </div>
         <input
-          type="checkbox"
-          className="sr-only"
-          checked={checked}
-          onChange={(e) => setChecked(e.target.checked)}
+          type="text"
+          className="w-full rounded-md bg-gray-700 mb-4 text-white px-4 py-2 border border-gray-600 focus:outline-none focus:border-blue-500"
+          placeholder="Nome do Chat"
+          value={chatName}
+          onChange={(e) => setChatName(e.target.value)}
         />
-        <label>Criar chat pdf?</label>
-        <div
-          className={`w-6 h-6 p-1 flex justify-center items-center mr-2 border ${
-            checked ? 'border-transparent bg-blue-600' : 'bg-white border-gray-400'
-          } rounded`}></div>
-      </div>
-    </BaseModal>
+      </BaseModal>
+    </>
   );
 };
 
