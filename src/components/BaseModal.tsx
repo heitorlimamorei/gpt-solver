989c8c3d43aaf2d8ed7d084557595da147257196
felipeRese <feliperese2018@gmail.com>
2024-05-17 18:58:49 -0300

    feat: chat selector

--- src/components/BaseModal.tsx
+++ src/components/BaseModal.tsx
@@ -4,27 +4,29 @@ interface IDarkModalProps {
   children: React.ReactNode;
   toggle(): void;
   handleSubmit(): void;
+  className: string;
+  buttons: boolean;
 }
 
-const BaseModal = ({ handleSubmit, toggle, children }: IDarkModalProps) => {
+const BaseModal = ({ buttons, className, handleSubmit, toggle, children }: IDarkModalProps) => {
   return (
     <div
-      className="fixed inset-0 z-40 flex items-center justify-center overflow-auto bg-black bg-opacity-50"
+      className={`fixed inset-0 z-40 flex items-center justify-center overflow-auto bg-black bg-opacity-50 ${className}`}
       onClick={toggle}>
       <div
         className="bg-gray-800 rounded-lg text-white overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full p-4"
         onClick={(e) => e.stopPropagation()}>
         {children}
-        <div className="flex justify-between space-x-2">
+        <div className={`justify-between space-x-2 ${buttons ? 'flex' : 'hidden'}`}>
           <button
             className="mt-2 px-5 py-2 bg-blue-500 hover:bg-blue-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
             onClick={handleSubmit}>
-            Salvar
+            Confirmar
           </button>
           <button
             className="mt-2 px-5 py-2 bg-red-600 hover:bg-red-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500"
             onClick={toggle}>
-            Fechar
+            Cancelar
           </button>
         </div>
       </div>
