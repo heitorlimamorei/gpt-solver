424b2f0aa90c326814732ac7f8b1804c599352a8
felipeRese <feliperese2018@gmail.com>
2024-05-18 19:39:39 -0300

    fix:optional props on BaseModal

--- src/components/BaseModal.tsx
+++ src/components/BaseModal.tsx
@@ -4,11 +4,17 @@ interface IDarkModalProps {
   children: React.ReactNode;
   toggle(): void;
   handleSubmit(): void;
-  className: string;
-  buttons: boolean;
+  className?: string;
+  buttons?: boolean;
 }
 
-const BaseModal = ({ buttons, className, handleSubmit, toggle, children }: IDarkModalProps) => {
+const BaseModal = ({
+  buttons = true,
+  className = '',
+  handleSubmit,
+  toggle,
+  children,
+}: IDarkModalProps) => {
   return (
     <div
       className={`fixed inset-0 z-40 flex items-center justify-center overflow-auto bg-black bg-opacity-50 ${className}`}
@@ -17,18 +23,20 @@ const BaseModal = ({ buttons, className, handleSubmit, toggle, children }: IDark
         className="bg-gray-800 rounded-lg text-white overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full p-4"
         onClick={(e) => e.stopPropagation()}>
         {children}
-        <div className={`justify-between space-x-2 ${buttons ? 'flex' : 'hidden'}`}>
-          <button
-            className="mt-2 px-5 py-2 bg-blue-500 hover:bg-blue-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-            onClick={handleSubmit}>
-            Confirmar
-          </button>
-          <button
-            className="mt-2 px-5 py-2 bg-red-600 hover:bg-red-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500"
-            onClick={toggle}>
-            Cancelar
-          </button>
-        </div>
+        {buttons && (
+          <div className="mt-4 flex justify-between space-x-2">
+            <button
+              className="px-5 py-2 bg-blue-500 hover:bg-blue-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              onClick={handleSubmit}>
+              Confirmar
+            </button>
+            <button
+              className="px-5 py-2 bg-red-600 hover:bg-red-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500"
+              onClick={toggle}>
+              Cancelar
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
