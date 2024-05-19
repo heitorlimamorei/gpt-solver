ee23653972fe64226e971f98a0cc72d3621a005c
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-05-19 14:58:26 -0300

    feat: implementation off usechatsetting in the components

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -3,19 +3,31 @@ import React from 'react';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 
+import useChatSettings from '@/hooks/useChatSettings';
 import { useFormat } from '@/hooks/useFormat';
 
-import Logo from '../../../public/ai.png';
+import defaultAssistantImage from '../../../public/ai.png';
+import financialControllerImage from '../../../public/fclogo.png';
 import profileImage from '../../../public/Profile.png';
 import { IMessage } from './Chat';
 
 export default function ChatMessage(message: IMessage) {
   const session = useSession();
 
+  const chatProperties = useChatSettings();
+
+  const assistantProps = chatProperties.getChatProperties();
+
   const image = session.data?.user?.image;
   const name = session.data?.user?.name;
 
+  const assistantImageRepository = {
+    defaultAssistantImage: defaultAssistantImage,
+    financialControllerImage: financialControllerImage,
+  };
+
   const formattedContent = useFormat(message);
+
   return (
     <li className="flex flex-col w-full px-3 mt-7">
       <div className="flex flex-row">
@@ -24,12 +36,14 @@ export default function ChatMessage(message: IMessage) {
           height={30}
           alt="Ai logo"
           src={
-            message.role === 'system' || message.role === 'assistant' ? Logo : image || profileImage
+            message.role === 'system' || message.role === 'assistant'
+              ? assistantProps.getImage(assistantImageRepository)
+              : image || profileImage
           }
           className="mr-5 rounded-full"
         />
         <div className="font-bold self-center bg-transparent">
-          {message.role === 'system' || message.role === 'assistant' ? 'AI' : name}
+          {message.role === 'system' || message.role === 'assistant' ? assistantProps.name : name}
         </div>
       </div>
       <ul className="mt-2 ml-[3rem] text-sm mx-2">
